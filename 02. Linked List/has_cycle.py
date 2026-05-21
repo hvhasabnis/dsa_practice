f274bb16c2faad2ca0fe62c4c2707850ab7051ec
Hrishikesh Vasant Hasabnis <hvhasabnis@csuchico.edu>
2026-05-20 22:22:19 -0700

    Provided solution for has cycle linked list problem

---
+++ 02. Linked List/has_cycle.py
@@ -0,0 +1,40 @@
+class ListNode:
+    def __init__(self, val=0, next: ListNode = None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def hasCycle(self, head: ListNode = None) -> bool:
+        if not head:
+            return True
+        
+        first = head
+        second = head.next
+
+        while not first or not second:
+            if first == second:
+                return False
+            
+            first = first.next
+            second = second.next.next
+
+        return True
+    
+def main():
+
+    node4 = ListNode(-4)
+    node3 = ListNode(0)
+    node2 = ListNode(2)
+    head = ListNode(3)
+
+    head.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node2
+
+    s = Solution()
+    isCycle = s.hasCycle(head)
+
+    print(isCycle)
+
+main()
